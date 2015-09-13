0f76abb705a3288ee8b951900a108108f16a02f2
manickyoj <hotshot354@gmail.com>
2015-09-13 16:10:40 -0400

    Finished regex exercise.
    Commiting the stack and hello programs which were added in as well.

---
+++ nfrancisci/ch2.5/find_track.c
@@ -0,0 +1,82 @@
+/* Example code for Software Systems at Olin College.
+
+Copyright 2014 Allen Downey
+License: Creative Commons Attribution-ShareAlike 3.0
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+#define NUM_TRACKS 5
+
+char tracks[][80] = {
+    "So What",
+    "Freddie Freeloader",
+    "Blue in Green",
+    "All Blues",
+    "Flamenco Sketches"
+};
+
+
+// Finds all tracks that contain the given string.
+//
+// Prints track number and title.
+void find_track(char search_for[])
+{
+    for (int i=0; i<NUM_TRACKS; i++) {
+	if (strstr(tracks[i], search_for)) {
+	    printf("Track %i: '%s'\n", i, tracks[i]);
+	}
+    }
+}
+
+// Finds all tracks that match the given pattern.
+//
+// Prints track number and title.
+void find_track_regex(char pattern[])
+{
+    regex_t* regex;
+    int status = regcomp(regex, pattern, REG_EXTENDED);
+
+    // Catch regex compilation errors, if any
+    if (status != 0) {
+        puts("Error in compiling regex!");
+        return;
+    }
+
+    int nomatch;
+    int num_matches = 1;
+    regmatch_t matches[num_matches];
+
+    for (int i=0; i < NUM_TRACKS; i++) {
+       nomatch = regexec(regex, tracks[i], num_matches, matches, 0);
+       if (!nomatch) printf("Track %i: '%s'\n", i, tracks[i]);
+    }
+}
+
+// Truncates the string at the first newline, if there is one.
+void rstrip(char s[])
+{
+    char *ptr = strchr(s, '\n');
+    if (ptr) {
+	*ptr = '\0';
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    char search_for[80];
+
+    /* take input from the user and search */
+    printf("Search for: ");
+    fgets(search_for, 80, stdin);
+    rstrip(search_for);
+
+    //find_track(search_for);
+    find_track_regex(search_for);
+
+    return 0;
+}
