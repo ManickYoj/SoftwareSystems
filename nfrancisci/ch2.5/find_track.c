8d4aa007856b538250d3a9c8ff83cae4e2ce7017
manickyoj <hotshot354@gmail.com>
2015-09-13 17:39:09 -0400

    Cleaned up the find_track regex function

--- nfrancisci/ch2.5/find_track.c
+++ nfrancisci/ch2.5/find_track.c
@@ -20,41 +20,38 @@ char tracks[][80] = {
     "Flamenco Sketches"
 };
 
-
-// Finds all tracks that contain the given string.
-//
-// Prints track number and title.
-void find_track(char search_for[])
-{
-    for (int i=0; i<NUM_TRACKS; i++) {
-	if (strstr(tracks[i], search_for)) {
-	    printf("Track %i: '%s'\n", i, tracks[i]);
-	}
-    }
-}
-
-// Finds all tracks that match the given pattern.
-//
-// Prints track number and title.
+/*
+ * Finds all tracks that match the given pattern.
+ * Prints track number and title.
+ *
+ * pattern: The regex pattern to match
+ */
 void find_track_regex(char pattern[])
 {
     regex_t* regex;
     int status = regcomp(regex, pattern, REG_EXTENDED);
 
-    // Catch regex compilation errors, if any
+    // Catch regex compilation errors, if any, and exit with error
     if (status != 0) {
         puts("Error in compiling regex!");
-        return;
+        regfree(regex);
+        exit(EXIT_FAILURE);
     }
 
+    // Declare variables needed to test 
     int nomatch;
     int num_matches = 1;
     regmatch_t matches[num_matches];
 
+    // Search all tracks for matches
     for (int i=0; i < NUM_TRACKS; i++) {
        nomatch = regexec(regex, tracks[i], num_matches, matches, 0);
+
+       // If a match occurs, print out the matched track name
        if (!nomatch) printf("Track %i: '%s'\n", i, tracks[i]);
     }
+
+    regfree(regex);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -75,7 +72,7 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    //find_track(search_for);
+    // Find all tracks that match the inputted pattern
     find_track_regex(search_for);
 
     return 0;
